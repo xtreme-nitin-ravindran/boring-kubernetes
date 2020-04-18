438084618fb24ab2c76f8a7cb660ad3d4d23914b
Nitin Ravindran <nravindran@pivotal.io>
2020-04-18 16:45:29 -0400

    three

--- tasks/compile-golang/task.sh
+++ tasks/compile-golang/task.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
+set -ex
+apt-get update
+apt-get -y install golang
 
-set -eux
+export GOROOT_BOOTSTRAP=/usr/local/go
 
 pushd golang
+    git checkout dev.boringcrypto.go1.14
     cd src
     ./all.bash
 popd
